d61a5503cca7f23dd3bc6a1cc3838088baf306d1
pranavkumar19926 <pranavdevula.199@gmail.com>
2026-07-10 23:58:46 +0530

    Time: 7 ms (60.07%), Space: 20.6 MB (52.48%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -1,47 +1,37 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right)
- *         : val(x), left(left), right(right) {}
- * };
- */
-
 class Solution {
 public:
 
     int count = 0;
 
-    // Counts all paths starting from 'root'
-    void ans(TreeNode* root, int target, long long add) {
+    void ans(TreeNode* root, int target, long long add,
+             unordered_map<long long, int>& mp) {
 
         if (root == NULL)
             return;
 
         add += root->val;
 
-        if (add == target)
-            count++;
+        long long val = add - target;
 
-        ans(root->left, target, add);
-        ans(root->right, target, add);
+        if (mp.count(val)) {
+            count += mp[val];
+        }
+
+        mp[add]++;
+
+        ans(root->left, target, add, mp);
+        ans(root->right, target, add, mp);
+
+        // Backtrack
+        mp[add]--;
     }
 
     int pathSum(TreeNode* root, int targetSum) {
 
-        if (root == NULL)
-            return 0;
+        unordered_map<long long, int> mp;
+        mp[0] = 1;
 
-        // Start a path from current node
-        ans(root, targetSum, 0);
-
-        // Try every node as starting node
-        pathSum(root->left, targetSum);
-        pathSum(root->right, targetSum);
+        ans(root, targetSum, 0, mp);
 
         return count;
     }
