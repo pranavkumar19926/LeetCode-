86b9e14182abce1a595bbe26c2c2923c545aff7b
pranavkumar19926 <pranavdevula.199@gmail.com>
2026-07-22 06:31:49 +0530

    Time: 35 ms (75.67%), Space: 64.6 MB (15.46%) - LeetHub

--- 1042-flower-planting-with-no-adjacent/1042-flower-planting-with-no-adjacent.cpp
+++ 1042-flower-planting-with-no-adjacent/1042-flower-planting-with-no-adjacent.cpp
@@ -1,112 +1,64 @@
 class Solution {
 public:
     vector<int> gardenNoAdj(int n, vector<vector<int>>& paths) {
-        //adjacy matrix
-        vector<vector<int>> adj(n+1 );
-        
-        for(int i=0 ; i< paths.size() ; i++){
-              
-              int u=paths[i][0];
-              int v=paths[i][1];
 
-              adj[u].push_back(v);
+        vector<vector<int>> adj(n + 1);
 
-              adj[v].push_back(u);
+        for (int i = 0; i < paths.size(); i++) {
 
+            int u = paths[i][0];
+            int v = paths[i][1];
 
-        }
-        
-
-        vector<int> parent(n+1 , -1);
-
-        vector<bool> visited(n+1 , false);
-
-        vector<int> ans(n,-1);
-
-      
-        for(int i=1 ; i<=n ; i++){
-               
-                  if(!visited[i]){
-                 queue<int> q;
-                
-                 q.push(i);
-                 visited[i]=true;
-                 while(!q.empty()){
-
-                      int node=q.front();
-
-                      q.pop();
-
-                    
-
-                     
-
-                      unordered_map<int,int> s;
-                      
-                      s[1]=0;
-                      s[2]=0;
-                      s[3]=0;
-                      s[4]=0;
-
-
-                      for(int i=0 ; i<adj[node].size() ; i++){
-
-                            if(!visited[adj[node][i]]){
-
-                                q.push(adj[node][i]);
-
-                                visited[adj[node][i]]=true;
-                            }
-
-                            
-
-                                if(parent[adj[node][i]]!=-1){
-                              s[parent[adj[node][i]] ]=1;
-
-                                }
-
-
-
-
-                      }
-
-                             
-
-                       
-                       for(auto it=s.begin() ; it!=s.end() ; it++){
-                                
-                                if(it->second!=1){
-                                      
-                                      parent[node]=it->first;
-                                      ans[node-1]=it->first;
-
-                                      break;
-
-                                }
-
-
-                       }
-                      
-
-
-
-                      
-
-                 }   
-
-
-                  }        
-
-
-
+            adj[u].push_back(v);
+            adj[v].push_back(u);
         }
 
+        vector<int> flower(n + 1, 0);
+        vector<int> ans(n);
 
+        vector<bool> visited(n + 1, false);
 
+        for (int i = 1; i <= n; i++) {
+
+            if (!visited[i]) {
+
+                queue<int> q;
+                q.push(i);
+                visited[i] = true;
+
+                while (!q.empty()) {
+
+                    int node = q.front();
+                    q.pop();
+
+                    bool used[5] = {false};
+
+                    for (int j = 0; j < adj[node].size(); j++) {
+
+                        int next = adj[node][j];
+
+                        if (!visited[next]) {
+                            visited[next] = true;
+                            q.push(next);
+                        }
+
+                        if (flower[next] != 0)
+                            used[flower[next]] = true;
+                    }
+
+                    for (int color = 1; color <= 4; color++) {
+
+                        if (!used[color]) {
+
+                            flower[node] = color;
+                            ans[node - 1] = color;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
 
         return ans;
-
-
-
     }
 };
