f13b5e833994991d2fdee7e5fe14756862964469
pranavkumar19926 <pranavdevula.199@gmail.com>
2026-07-23 08:59:32 +0530

    Time: 7 ms (95.37%), Space: 26.1 MB (47.76%) - LeetHub

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -1,160 +1,135 @@
 class Solution {
 public:
 
-bool check(int row , int col , int rmax , int cmax , vector<vector<int>> & grid){
-         if(row>=0 && col >=0 && row<rmax && col<cmax){
-                    return true;
-         }
-         else{
-            return false;
-         }
+bool check(int row , int col , int rmax , int cmax){
+
+    if(row>=0 && col>=0 && row<rmax && col<cmax){
+        return true;
+    }
+
+    return false;
 }
 
-    int minCost(vector<vector<int>>& grid) {
+int minCost(vector<vector<int>>& grid) {
 
-               int n=grid.size();
-               int m=grid[0].size();
+    int n=grid.size();
+    int m=grid[0].size();
 
-              // FIX 1: Use a dist array instead of modifying grid values
-              vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
+    vector<vector<int>> dist(n,vector<int>(m,INT_MAX));
 
-              // FIX 2: Use deque instead of queue for 0-1 BFS
-              deque<pair<int, pair<int,int> >> q;
+    deque<pair<int,int>> q;
 
-              q.push_back(make_pair(0,make_pair(0,0) ));
+    q.push_front(make_pair(0,0));
 
-              dist[0][0]=0;
+    dist[0][0]=0;
 
-         while(!q.empty() ){
-            
-            int c=q.front().first;
-            int i=q.front().second.first;
-            int j=q.front().second.second;
+    while(!q.empty()){
 
-            q.pop_front();
+        int i=q.front().first;
+        int j=q.front().second;
 
-            if(i==n-1 && j==m-1){
-                   return c;
+        q.pop_front();
+
+        int c=dist[i][j];
+
+        if(grid[i][j]==1){
+
+            if(check(i,j+1,n,m) && c<dist[i][j+1]){
+                dist[i][j+1]=c;
+                q.push_front(make_pair(i,j+1));
             }
 
-            // Skip stale entries
-            if(c > dist[i][j]) continue;
-
-
-            if(grid[i][j]==1){
-
-                   if(check(i, j+1 , n , m , grid)){
-                        if(c < dist[i][j+1]){
-                              dist[i][j+1]=c;
-                              q.push_front(make_pair(c,make_pair(i,j+1) ));
-                        }
-                   }
-                   if(check(i, j-1 , n , m , grid)){
-                        if(c+1 < dist[i][j-1]){
-                              dist[i][j-1]=c+1;
-                              q.push_back(make_pair(c+1,make_pair(i,j-1) ));
-                        }
-                   }
-                   if(check(i+1, j , n , m , grid)){
-                        if(c+1 < dist[i+1][j]){
-                              dist[i+1][j]=c+1;
-                              q.push_back(make_pair(c+1,make_pair(i+1,j) ));
-                        }
-                   }
-                   if(check(i-1, j , n , m , grid)){
-                        if(c+1 < dist[i-1][j]){
-                              dist[i-1][j]=c+1;
-                              q.push_back(make_pair(c+1,make_pair(i-1,j) ));
-                        }
-                   }
-            }
-            
-
-            else if(grid[i][j]==2){
-                   
-                   if(check(i, j+1 , n , m , grid)){
-                        if(c+1 < dist[i][j+1]){
-                              dist[i][j+1]=c+1;
-                              q.push_back(make_pair(c+1,make_pair(i,j+1) ));
-                        }
-                   }
-                   if(check(i, j-1 , n , m , grid)){
-                        if(c < dist[i][j-1]){
-                              dist[i][j-1]=c;
-                              q.push_front(make_pair(c,make_pair(i,j-1) ));
-                        }
-                   }
-                   if(check(i+1, j , n , m , grid)){
-                        if(c+1 < dist[i+1][j]){
-                              dist[i+1][j]=c+1;
-                              q.push_back(make_pair(c+1,make_pair(i+1,j) ));
-                        }
-                   }
-                   if(check(i-1, j , n , m , grid)){
-                        if(c+1 < dist[i-1][j]){
-                              dist[i-1][j]=c+1;
-                              q.push_back(make_pair(c+1,make_pair(i-1,j) ));
-                        }
-                   }
+            if(check(i,j-1,n,m) && c+1<dist[i][j-1]){
+                dist[i][j-1]=c+1;
+                q.push_back(make_pair(i,j-1));
             }
 
-            else if(grid[i][j]==3){
-                        
-                   if(check(i, j+1 , n , m , grid)){
-                        if(c+1 < dist[i][j+1]){
-                              dist[i][j+1]=c+1;
-                              q.push_back(make_pair(c+1,make_pair(i,j+1) ));
-                        }
-                   }
-                   if(check(i, j-1 , n , m , grid)){
-                        if(c+1 < dist[i][j-1]){
-                              dist[i][j-1]=c+1;
-                              q.push_back(make_pair(c+1,make_pair(i,j-1) ));
-                        }
-                   }
-                   if(check(i+1, j , n , m , grid)){
-                        if(c < dist[i+1][j]){
-                              dist[i+1][j]=c;
-                              q.push_front(make_pair(c,make_pair(i+1,j) ));
-                        }
-                   }
-                   if(check(i-1, j , n , m , grid)){
-                        if(c+1 < dist[i-1][j]){
-                              dist[i-1][j]=c+1;
-                              q.push_back(make_pair(c+1,make_pair(i-1,j) ));
-                        }
-                   }
+            if(check(i+1,j,n,m) && c+1<dist[i+1][j]){
+                dist[i+1][j]=c+1;
+                q.push_back(make_pair(i+1,j));
             }
 
-            else{
-
-                   if(check(i, j+1 , n , m , grid)){
-                        if(c+1 < dist[i][j+1]){
-                              dist[i][j+1]=c+1;
-                              q.push_back(make_pair(c+1,make_pair(i,j+1) ));
-                        }
-                   }
-                   if(check(i, j-1 , n , m , grid)){
-                        if(c+1 < dist[i][j-1]){
-                              dist[i][j-1]=c+1;
-                              q.push_back(make_pair(c+1,make_pair(i,j-1) ));
-                        }
-                   }
-                   if(check(i+1, j , n , m , grid)){
-                        if(c+1 < dist[i+1][j]){
-                              dist[i+1][j]=c+1;
-                              q.push_back(make_pair(c+1,make_pair(i+1,j) ));
-                        }
-                   }
-                   if(check(i-1, j , n , m , grid)){
-                        if(c < dist[i-1][j]){
-                              dist[i-1][j]=c;
-                              q.push_front(make_pair(c,make_pair(i-1,j) ));
-                        }
-                   }
+            if(check(i-1,j,n,m) && c+1<dist[i-1][j]){
+                dist[i-1][j]=c+1;
+                q.push_back(make_pair(i-1,j));
             }
-         }
 
-       return dist[n-1][m-1];
+        }
+
+        else if(grid[i][j]==2){
+
+            if(check(i,j+1,n,m) && c+1<dist[i][j+1]){
+                dist[i][j+1]=c+1;
+                q.push_back(make_pair(i,j+1));
+            }
+
+            if(check(i,j-1,n,m) && c<dist[i][j-1]){
+                dist[i][j-1]=c;
+                q.push_front(make_pair(i,j-1));
+            }
+
+            if(check(i+1,j,n,m) && c+1<dist[i+1][j]){
+                dist[i+1][j]=c+1;
+                q.push_back(make_pair(i+1,j));
+            }
+
+            if(check(i-1,j,n,m) && c+1<dist[i-1][j]){
+                dist[i-1][j]=c+1;
+                q.push_back(make_pair(i-1,j));
+            }
+
+        }
+
+        else if(grid[i][j]==3){
+
+            if(check(i,j+1,n,m) && c+1<dist[i][j+1]){
+                dist[i][j+1]=c+1;
+                q.push_back(make_pair(i,j+1));
+            }
+
+            if(check(i,j-1,n,m) && c+1<dist[i][j-1]){
+                dist[i][j-1]=c+1;
+                q.push_back(make_pair(i,j-1));
+            }
+
+            if(check(i+1,j,n,m) && c<dist[i+1][j]){
+                dist[i+1][j]=c;
+                q.push_front(make_pair(i+1,j));
+            }
+
+            if(check(i-1,j,n,m) && c+1<dist[i-1][j]){
+                dist[i-1][j]=c+1;
+                q.push_back(make_pair(i-1,j));
+            }
+
+        }
+
+        else{
+
+            if(check(i,j+1,n,m) && c+1<dist[i][j+1]){
+                dist[i][j+1]=c+1;
+                q.push_back(make_pair(i,j+1));
+            }
+
+            if(check(i,j-1,n,m) && c+1<dist[i][j-1]){
+                dist[i][j-1]=c+1;
+                q.push_back(make_pair(i,j-1));
+            }
+
+            if(check(i+1,j,n,m) && c+1<dist[i+1][j]){
+                dist[i+1][j]=c+1;
+                q.push_back(make_pair(i+1,j));
+            }
+
+            if(check(i-1,j,n,m) && c<dist[i-1][j]){
+                dist[i-1][j]=c;
+                q.push_front(make_pair(i-1,j));
+            }
+
+        }
+
     }
+
+    return dist[n-1][m-1];
+}
 };
