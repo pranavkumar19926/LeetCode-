77f4952abd07e7603b0ce8914ae0b5367a328d31
pranavkumar19926 <pranavdevula.199@gmail.com>
2026-08-11 17:46:05 +0530

    Time: 8 ms (79.13%), Space: 14.2 MB (75.59%) - LeetHub

--- 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.cpp
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.cpp
@@ -1,57 +1,23 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* left;
-    Node* right;
-    Node* parent;
-};
-*/
-
 class Solution {
 public:
-        
-        
-     
+    Node* lowestCommonAncestor(Node* p, Node* q) {
 
+        Node* a = p;
+        Node* b = q;
 
-    Node* lowestCommonAncestor(Node* p, Node * q) {
-               unordered_set<Node*> s;
+        while (a != b) {
 
-               Node* t=p;
+            if (a == NULL)
+                a = q;
+            else
+                a = a->parent;
 
+            if (b == NULL)
+                b = p;
+            else
+                b = b->parent;
+        }
 
-               while(t !=NULL){
-
-                     s.insert(t);
-
-                     t=t->parent;
-
-
-               }
-
-
-               Node* a=q;
-
-               Node* ans=NULL;
-
-
-               while(a!=NULL){
-
-                        if(s.count(a)){
-
-                             ans=a;
-                             break;
-                        }
-                        else{
-
-                            a=a->parent;
-                        }
-               }
-
-
-               return ans;
-
+        return a;
     }
 };
