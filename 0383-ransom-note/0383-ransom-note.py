e139e8dd7a1861acf5691f2ee61575cfdcbe5ef6
pranavkumar19926 <pranavdevula.199@gmail.com>
2026-09-03 15:49:47 +0530

    Time: 10 ms (91.98%), Space: 19.6 MB (34.02%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,21 +1,36 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
 
-        ransomNote = ''.join(sorted(ransomNote))
-        magazine = ''.join(sorted(magazine))
+        vec={}
 
-        sz = len(ransomNote)
-        first = 0
+        sz=len(ransomNote)
+
+        for i in ransomNote:
+
+            if i in vec:
+
+                vec[i]+=1
+            else:
+                vec[i]=1
+            
+
 
         for i in magazine:
 
-            if first >= sz:
-                break
+            if sz==0:
+                return True
 
-            if ransomNote[first] == i:
-                first += 1
+            if i in vec and vec[i] > 0:
 
-        if first >= sz:
+                sz-=1
+                vec[i]-=1
+            
+        if sz == 0:
             return True
         else:
-            return False
+
+            return False    
+
+
+
+       
